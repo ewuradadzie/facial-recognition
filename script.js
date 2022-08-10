3bc0d2cd25485c880ff4e0b498c8865ad1edf3d7
Ewuradjoa Dadzie <ewura.dadzie@gmail.com>
2022-08-10 16:01:50 +0000

    edits

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ewuradadzie/live-face-recognition/dev/labeled_images/${label}/${i}.jpg`)
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ewuradadzie/live-face-recognition/master/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
