fb2778e2a77ad6d58f9bc2e0c82aa42e1c7f21ee
Ewuradjoa Dadzie <ewura.dadzie@gmail.com>
2023-01-17 17:00:38 +0000

    commit

--- script.js
+++ script.js
@@ -4,43 +4,44 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start)
+]).then(startVideo)
 
-async function start() {
-
+async function startVideo() {
   navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
   )
 
+  
+}
+
+video.addEventListener('play', async () => {
   const container = document.createElement('div')
   container.style.position = 'relative'
   document.body.append(container)
+  container.append(video)
+
   const labeledFaceDescriptors = await loadLabeledImages()
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-  let image
-  let canvas
-  document.body.append('Loaded')
-  video.addEventListener('play', async () => {
-    // if (image) image.remove()
-    if (canvas) canvas.remove()
-    // image = await faceapi.bufferToImage(imageUpload.files[0])
-    container.append(video)
-    canvas = faceapi.createCanvasFromMedia(video)
-    container.append(canvas)
-    const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
+  console.log('Loaded')
+  
+  canvas = faceapi.createCanvasFromMedia(video)
+  container.append(canvas)
+
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+
+  
+  const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+  const resizedDetections = faceapi.resizeResults(detections, displaySize)
+  const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+  results.forEach((result, i) => {
+    const box = resizedDetections[i].detection.box
+    const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+    drawBox.draw(canvas)
   })
-}
+})
 
 function loadLabeledImages() {
   const labels = ['EKD']
